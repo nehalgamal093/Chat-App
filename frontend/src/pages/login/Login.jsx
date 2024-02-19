0a531f6d952197f7accf146a204e3b3dc04e440d
nehalgamal093 <nehalgamal093@gmail.com>
2024-02-19 20:40:26 +0200

    authentication and localstorage

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 
 const Login = () => {
@@ -23,9 +24,9 @@ const Login = () => {
                             placeholder="Enter Password"
                             className="w-full input input-bprdered h-10" />
                     </div>
-                    <a href="#" className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block">
+                    <Link to={"/signup"} className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block">
                         {"Don't"} have an account?
-                    </a>
+                    </Link>
                     <div>
                         <button className="btn btn-block btn-sm mt-2">Login</button>
                     </div>
