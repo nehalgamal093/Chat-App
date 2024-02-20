cac5df789caec9cb420aba60d6596086705b5d28
nehalgamal093 <nehalgamal093@gmail.com>
2024-02-20 18:22:45 +0200

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import cookieParser from "cookie-parser";
-
-const app = express();
+import { app, server } from "./socket/socket.js";
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 dotenv.config();
 app.use(express.json());
@@ -15,11 +16,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
-app.get("/", (req, res) => {
-  res.send("Hello world!!");
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Server running on port ${PORT}!!`);
 });
