d59b3cbb60e25a590c5110c74ef757d1755d068d
nehalgamal093 <nehalgamal093@gmail.com>
2024-02-20 14:49:16 +0200

    send messages

---
+++ frontend/src/utils/emojis.js
@@ -0,0 +1,15 @@
+export const funEmojis = [
+  "🐇 ",
+  "😻 ",
+  "💌 ",
+  "🧶 ",
+  "🤠 ",
+  "🤦 ",
+  "🛑 ",
+  "🐙 ",
+  "🤞",
+  "💃 ",
+];
+export const getRandomEmoji = () => {
+  return funEmojis[Math.floor(Math.random() * funEmojis.length)];
+};
