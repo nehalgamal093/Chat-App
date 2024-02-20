beab1bebcafd17d890ae92aa59afd4ecc775cc1c
nehalgamal093 <nehalgamal093@gmail.com>
2024-02-20 13:47:23 +0200

    routes

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -57,7 +57,9 @@ const SignUp = () => {
                         Already have an account?
                     </Link>
                     <div>
-                        <button className="btn btn-block btn-sm mt-2">Sign Up</button>
+                        <button className="btn btn-block btn-sm mt-2" disabled={loading}>
+                            {loading ? <span className="loading loading-spinner"></span> : "Sign Up"}
+                        </button>
                     </div>
                 </form>
             </div>
