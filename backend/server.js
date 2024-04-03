788086784d8d471977b00878b83ae67e37cce79e
nehalgamal093 <nehalgamal093@gmail.com>
2024-04-03 16:19:54 +0200

    fcm notification

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { json } from "express";
 import dotenv from "dotenv";
 import path from "path";
 import authRoutes from "./routes/auth.routes.js";
@@ -7,12 +7,43 @@ import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 // import cookieParser from "cookie-parser";
 import { app, server } from "./socket/socket.js";
+import { initializeApp, applicationDefault } from "firebase-admin/app";
+import { getMessaging } from "firebase-admin/messaging";
+
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 dotenv.config();
 app.use(express.json());
 // app.use(cookieParser());
+initializeApp({
+  credential: applicationDefault(),
+  projectId: process.env.PROJECT_ID,
+});
+app.post("/notification", function (req, res) {
+  const receivedToken = req.body.fcmToken;
+  const message = {
+    notification: {
+      title: "Notification",
+      body: "This is a test notification",
+    },
+    token: receivedToken,
+  };
+  getMessaging()
+    .send(message)
+    .then((response) => {
+      res.status(200).json({
+        message: "Successfully sent Message",
+        token: receivedToken,
+      });
+      console.log("Successfully sent message");
+    })
+    .catch((error) => {
+      res.status(400);
+      res.send(error);
+      console.log("Error sending message", error);
+    });
+});
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
